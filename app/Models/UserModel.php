e363bd0866de0136b792f21d91b89d1a42dc2e8c
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-03-05 09:18:41 +0700

    Pertemuan 4 Praktikum 1 - $fillable:

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,8 @@ class UserModel extends Model
 
     protected $table = 'm_users';
     protected $primarykey = 'user_id';
+
+    // @var array
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
