2140299391aaaaecce71c3cf15c0807a2e4a8d20
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-03-05 11:02:45 +0700

    Pertemuan 4 Praktikum 2.4 – Retreiving or Creating Models

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -13,6 +13,6 @@ class UserModel extends Model
     protected $primaryKey = 'user_id';
 
     // @var array
-    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    // protected $fillable = ['level_id', 'username', 'nama'];
 }
