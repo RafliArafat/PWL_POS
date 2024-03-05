bcd975883e7f5cefd4cb6aa6f993b47d565c156b
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-03-05 10:23:47 +0700

    Pertemuan 4 Praktikum 2.1 – Retrieving Single Models

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,7 +10,7 @@ class UserModel extends Model
     use HasFactory;
 
     protected $table = 'm_users';
-    protected $primarykey = 'user_id';
+    protected $primaryKey = 'user_id';
 
     // @var array
     // protected $fillable = ['level_id', 'username', 'nama', 'password'];
