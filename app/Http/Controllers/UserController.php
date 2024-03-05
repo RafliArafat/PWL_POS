769d6756de38a7207337d7ba30a1be48533a81a6
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-03-05 10:42:38 +0700

    Pertemuan 4 Praktikum 2.3 – Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,7 +47,11 @@ class UserController extends Controller
 
         // $user = UserModel::findOrFail(1);
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        
+        $user = UserModel::where('level_id', 2)->count();
+        
+        // dd($user); dd = dump or die, digunakan untuk menghentikan skrip dan mencetak nilai yang diberikan
 
 
         return view('user', ['data' => $user]);
